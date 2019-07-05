34c0d7e3837f016cd68b19c142a5235e55125858
DEIVINNI <41592653+DEIVINNI@users.noreply.github.com>
2019-07-05 17:38:44 -0300

    Update serviceAccount.js

--- src/util/assets/serviceAccount.js
+++ src/util/assets/serviceAccount.js
@@ -1,9 +1,9 @@
 module.exports = {
-  apiKey: "AIzaSyBoyvv-hz4E9wOZotKLJblZUGtqW0pGgaE",
-  authDomain: "kagamihara-46bb9.firebaseapp.com",
-  databaseURL: "https://kagamihara-46bb9.firebaseio.com",
-  projectId: "kagamihara-46bb9",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
   storageBucket: "",
-  messagingSenderId: "293856501180",
-  appId: "1:293856501180:web:b3553b3ce74b6291"
+  messagingSenderId: "",
+  appId: ""
 }
