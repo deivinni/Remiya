1dede93f6ac666ef67c34835ecd400d5497a58eb
DEIVINNI <41592653+DEIVINNI@users.noreply.github.com>
2019-07-19 13:26:46 -0300

    Added support for shard

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 require('ylenv').load();
-module.exports = require('./src/main.js');
+const { ShardingManager } = require('discord.js');
+const shards = new ShardingManager('./src/main.js', {
+    totalShards: 'auto',
+    respawn: true,
+    token: process.env.TOKEN
+});
+
+shards.spawn();
+shards.on('launch', async shard => {
+    try { console.log(`[SHARD] - Shard: ${shard.id+1} carregado.`) } 
+    catch (e) { console.error(e) }
+});
