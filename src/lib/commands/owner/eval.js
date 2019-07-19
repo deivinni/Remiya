752194f750333f93ebd641642cb52a2b5e8856d8
DEIVINNI <41592653+DEIVINNI@users.noreply.github.com>
2019-07-19 00:14:01 -0300

    ¯\_(ツ)_/¯

--- src/lib/commands/owner/eval.js
+++ src/lib/commands/owner/eval.js
@@ -1,6 +1,6 @@
 module.exports = {
     run: async(msg) => {
-        let toEval = msg.args.join(' ').replace('```','').replace('```js','');
+        let toEval = msg.args.join(' ');
         if (toEval.includes('msg.bot.token') || toEval.includes('msg.guild.leave()')) {
             msg.channel.send(`${msg.config.e_men.errado} \`|\` ${msg.author}, talvez um outro dia.`);
         } else {
@@ -12,18 +12,16 @@ module.exports = {
                     let time_start = process.hrtime()
                     let time_diff = process.hrtime(time_start)
                     let executed = `${time_diff[0] > 0 ? +'`'+time_diff[0]+'`s' : ''}\`${time_diff[1] / 1000000}\`ms`
-                    msg.channel.send(`\`\`\`js\n${evaluated}\`\`\``, {maxLenght: 2000}) && 
-                    msg.author.send(
-                        `**Executado em**: ${executed}\n`+
-                        `**Tipo**: ${typeof evaluated}\n`+
-                        `**Resultado**: \`\`\`js\n${evaluated}\`\`\``, {
-                            maxLenght: 2000
-                        }
-                    )
+                    msg.channel.send(evaluated, { code: 'js', maxLenght: 2000 }) && 
+                    msg.author.send([
+                        `**Executado em**: ${executed}`,
+                        `**Tipo**: ${typeof evaluated}`,
+                        `**Resultado**: \`\`\`js\n${evaluated}\`\`\``
+                    ].join('\n'), { maxLenght: 2000 })
                 }
             } catch (e) {
-                msg.channel.send(`\`\`\`js\n${e}\n\`\`\``) && 
-                msg.author.send(`\`\`\`js\n${e.stack}\n\`\`\``, {maxLenght: 2000});
+                msg.channel.send(e, { code:'js' }) && 
+                msg.author.send(e.stack, { code: 'js', maxLenght: 2000 });
             }
         }
     },
